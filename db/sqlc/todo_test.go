563e55539493a257389b82a6215b0d73dd8665b0
jeerapat <jeerapat@mfec.co.th>
2023-03-13 10:34:39 +0700

    add test list todos

--- db/sqlc/todo_test.go
+++ db/sqlc/todo_test.go
@@ -3,12 +3,13 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/jsritawan/simple-todo-example/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateTodo(t *testing.T) {
+func createRandomTodo(t *testing.T) Todo {
 	// Arrange
 	note := util.RandomTodoNote()
 
@@ -23,4 +24,30 @@ func TestCreateTodo(t *testing.T) {
 
 	require.NotZero(t, todo.ID)
 	require.NotZero(t, todo.CreateAt)
+
+	return todo
+}
+
+func TestCreateTodo(t *testing.T) {
+	createRandomTodo(t)
+}
+
+func TestListTodo(t *testing.T) {
+	// Arrange
+	todo := createRandomTodo(t)
+
+	// Act
+	todos, err := testQuery.ListTodos(context.Background())
+
+	// Assert
+	require.NoError(t, err)
+	require.Greater(t, len(todos), 0)
+
+	latestTodo := todos[0]
+	require.Equal(t, todo.ID, latestTodo.ID)
+	require.Equal(t, todo.Note, latestTodo.Note)
+	require.Equal(t, todo.Completed, latestTodo.Completed)
+	require.WithinDuration(t, todo.CreateAt, latestTodo.CreateAt, time.Second*1)
+	require.Equal(t, todo.UpdateAt, latestTodo.UpdateAt)
+	require.Equal(t, todo.DeleteAt, latestTodo.DeleteAt)
 }
